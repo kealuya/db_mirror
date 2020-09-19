5f9f6383e63a685417110a6f331553e765ce24ba
kealuya <kealuya@126.com>
2020-09-20 00:36:44 +0800

    初始化 UI

--- test.go
+++ test.go
@@ -1,25 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"db_mirror/db"
+	"fmt"
 )
 
 type ss struct {
 	a string
 	b int
 }
+type NEIKONG_CL_EMPLOYEE_INFO struct {
+	ID          string `xorm:"ID"`
+	USERID      string `xorm:"USERID"`
+	PASSWORD    string `xorm:"PASSWORD"`
+	CHINESENAME string `xorm:"CHINESENAME"`
+}
 
-
+func (NEIKONG_CL_EMPLOYEE_INFO) TableName() string {
+	return "NEIKONG_CL_EMPLOYEE_INFO"
+}
 func main() {
 
-	db  := db.NewHandlerDb(3)
-	en:=db.Engine
-	r,err:= en.SQL("select * from ddd").QueryString()
+	//nce := NEIKONG_CL_EMPLOYEE_INFO{
+	//	USERID:      "33333",
+	//	PASSWORD:    "2222",
+	//	CHINESENAME: "买买买",
+	//}
+	//
+	db := db.NewHandlerDb(4)
+	en := db.Engine
+	//a, err := en.Insert(&nce)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//
+	//fmt.Println("affect=", a, nce.ID)
+
+	r, err := en.SQL("SELECT EMPLOYEE_INFO_AUTOINC.NEXTVAL FROM dual ").QueryString()
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println(err.Error())
 	}
-
 	fmt.Println(r)
 }
